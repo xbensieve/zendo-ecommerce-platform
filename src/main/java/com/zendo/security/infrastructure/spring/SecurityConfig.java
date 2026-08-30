caddd038f102d916bcae355f961ee3995806a9e7
xbensieve <bennguyen.contact@gmail.com>
2026-08-30 11:26:44 +0700

    config swagger

--- src/main/java/com/zendo/security/infrastructure/spring/SecurityConfig.java
+++ src/main/java/com/zendo/security/infrastructure/spring/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/login", "/auth/register", "/error").permitAll()
                 .requestMatchers("/actuator/**").permitAll()
+                .requestMatchers("/v3/api-docs", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .requestMatchers(HttpMethod.GET, "/catalog/**").permitAll() // Example: Public read
                 .requestMatchers(HttpMethod.GET, "/api/products/*/reviews/**").permitAll()
                 .anyRequest().authenticated()
